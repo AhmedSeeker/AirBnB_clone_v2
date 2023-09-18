ed25dd990fc775fe18ddd7c186f03a6083eb0721
Ahmed <rifkiahmed.91@gmail.com>
2023-09-18 14:53:24 -0700

    Add implementation for database storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,22 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (Column, String, DateTime)
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow())
+    updated_at = Column(DateTime, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        from models import storage
-        self.id = str(uuid.uuid4())
+        if 'id' not in kwargs:
+            self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if 'updated_at' in kwargs:
@@ -20,7 +28,6 @@ class BaseModel:
         if '__class__' in kwargs:
             del kwargs['__class__']
         self.__dict__.update(kwargs)
-        storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -31,7 +38,13 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
+
+    def delete(self):
+        """Delete the current instance"""
+        from models import storage
+        storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -41,4 +54,7 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
+        print(dictionary)
         return dictionary

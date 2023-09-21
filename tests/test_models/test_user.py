3349d7656165ab120d03226b460ca9d14d64b804
eddianiikbal <eddianiiqbal@gmail.com>
2023-09-20 17:14:38 -0700

    Test cases

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
 from models.user import User
+import unittest
 
 
-class test_User(test_basemodel):
+class test_User(unittest.TestCase):
     """ """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
+    def setUp(self):
+        """First call"""
+        self.user = User("aa", "bb", "cc", "dd")
 
     def test_first_name(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        self.user = User()
+        self.assertEqual(self.user.first_name, "")
 
     def test_last_name(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        self.assertEqual(type(self.user.last_name), str)
 
     def test_email(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.email), str)
+        self.assertEqual(type(self.user.email), str)
 
     def test_password(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.password), str)
+        self.assertEqual(type(user.password), str)

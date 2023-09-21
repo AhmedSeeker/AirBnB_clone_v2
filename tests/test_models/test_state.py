ac16a39af2ba3940f1e489e1235099a3e553412e
eddianiikbal <eddianiiqbal@gmail.com>
2023-09-20 17:14:29 -0700

    Test cases

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -4,16 +4,15 @@ from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
-class test_state(test_basemodel):
+class test_state(unittest.TestCase):
     """ """
 
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
         self.name = "State"
-        self.value = State
+        self.value = State()
 
     def test_name3(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(self.value.name), str)

2fc4f02e6b1d689291b253711fcd2708346f2940
ahmed <rifkiahmed.91@gmail.com>
2023-09-23 21:31:34 +0000

    Test a new code

--- models/place.py
+++ models/place.py
@@ -50,7 +50,7 @@ class Place(BaseModel, Base):
             """Return all amenities related to the current place"""
             amenities = []
             for amenity in storage.all(Amenity):
-                if amenity.place_id in self.amenity_ids:
+                if amenity.ids in self.amenity_ids:
                     amenities.append(amenity)
             return amenities
 

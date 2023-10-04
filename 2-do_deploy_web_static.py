f066ef02ff08046f77915e9b519078ec6b7539f0
ahmed <rifkiahmed.91@gmail.com>
2023-10-04 22:21:03 +0000

    Deploy web static to remote servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,23 +6,23 @@ from fabric.api import put, run, env
 from os import path
 
 env.hosts = ['100.25.34.143', '54.85.11.239']
-env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
     """Distribute an archive to web servers"""
-    if not path.isfile(archive_path):
+    if path.isfile(archive_path):
+        file = archive_path.split("/")[-1]
+        filename = file.split(".")[0]
+        put(archive_path, f"/tmp/{file}")
+        folder = "/data/web_static/releases"
+        run(f"mkdir -p {folder}/{filename}/")
+        run(f"tar -xzf /tmp/{file} -C {folder}/{filename}/")
+        run(f"rm /tmp/{file}")
+        run(f"mv {folder}/{filename}/web_static/* {folder}/{filename}/")
+        run(f"rm -rf {folder}/{filename}/web_static")
+        run(f"rm -rf /data/web_static/current")
+        run(f"ln -s {folder}/{filename}/ /data/web_static/current")
+        print("New version deployed!")
+        return True
+    else:
         return False
-    file = archive_path.split("/")[-1]
-    filename = file.split(".")[0]
-    put(archive_path, f"/tmp/{file}")
-    folder = "/data/web_static/releases"
-    run(f"mkdir -p {folder}/{filename}/")
-    run(f"tar -xzf /tmp/{file} -C {folder}/{filename}/")
-    run(f"rm /tmp/{file}")
-    run(f"mv {folder}/{filename}/web_static/* {folder}/{filename}/")
-    run(f"rm -rf {folder}/{filename}/web_static")
-    run(f"rm -rf /data/web_static/current")
-    run(f"ln -s {folder}/{filename}/ /data/web_static/current")
-    print("New version deployed!")
-    return True

fe1dcf6fa33febfc78226dbb10bc58a20c9a2d7e
ahmed <rifkiahmed.91@gmail.com>
2023-10-04 23:05:35 +0000

    Deploy web static to remote servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,12 +2,31 @@
 """ Fabric script that generates a .tgz archive from the contents
     of the web_static folder of my AirBnB Clone repo
 """
-from fabric.api import put, run, env
+from fabric.api import put, run, env, local
+from datetime import datetime
 from os import path
+
+
+def do_pack():
+    """ Generate a .tgz archive from the contents of the web_static folder """
+
+    """folder to store all archives"""
+    local("mkdir -p versions")
+
+    """archive name: web_static_<year><month><day><hour><minute><second>.tgz"""
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+    archive = f"versions/web_static_{timestamp}.tgz"
+    try:
+        local(f"tar -cvzf {archive} web_static")
+        return archive
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
     """Distribute an archive to web servers"""
+
     if path.isfile(archive_path):
         env.hosts = ['100.25.34.143', '54.85.11.239']
         file = archive_path.split("/")[-1]

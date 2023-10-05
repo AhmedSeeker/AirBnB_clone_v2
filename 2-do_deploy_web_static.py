052856106f51342c84876eebafe1ab8f4885bcf0
ahmed <rifkiahmed.91@gmail.com>
2023-10-05 01:07:15 +0000

    Delete archive from remote server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -31,23 +31,22 @@ def do_deploy(archive_path):
     if path.exists(archive_path):
         file = archive_path.split("/")[-1]
         filename = file.split(".")[0]
-        if put(archive_path, f"/tmp/").failed:
+        if put(archive_path, f"/tmp/{file}").failed:
             return False
-        folder = "/data/web_static/releases"
-        if run(f"mkdir -p {folder}/{filename}/").failed:
+        folder = "/data/web_static/releases/{filename}/"
+        if run(f"mkdir -p {folder}").failed:
             return False
-        if run(f"tar -xzf /tmp/{file} -C {folder}/{filename}/").failed:
+        if run(f"tar -xzf /tmp/{file} -C {folder}").failed:
             return False
-        if run(f"rm /tmp/{archive_path}").failed:
+        if run(f"rm /tmp/{file}").failed:
             return False
-        if run(f"mv {folder}/{filename}/web_static/* \
-                {folder}/{filename}/").failed:
+        if run(f"mv {folder}/web_static/* {folder}").failed:
             return False
-        if run(f"rm -rf {folder}/{filename}/web_static").failed:
+        if run(f"rm -rf {folder}/web_static").failed:
             return False
         if run(f"rm -rf /data/web_static/current").failed:
             return False
-        if run(f"ln -s {folder}/{filename}/ /data/web_static/current").failed:
+        if run(f"ln -s {folder} /data/web_static/current").failed:
             return False
         print("New version deployed!")
         return True

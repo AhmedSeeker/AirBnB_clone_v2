25e5d87e9ca1304c35bc4253f2b408bb1ef33bfd
ahmed <rifkiahmed.91@gmail.com>
2023-10-06 23:49:30 +0000

    Fix checks

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,48 +1,50 @@
-#Redo task 0 by using Puppet
+#using Puppet
 
-exec {'directories':
-  command  => 'sudo mkdir -p /data/web_static/releases/test/; sudo mkdir -p /data/web_static/shared/',
+exec {'apt_update':
+  command  => 'sudo apt update',
   provider => shell,
-}
-
--> file {'/data/web_static/releases/test/index.html':
-  ensure  => file,
-  content => "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n",
-  require => Exec['directories'],
-}
-
-file {'/data/web_static/current':
-  ensure  => link,
-  target  => '/data/web_static/releases/test',
-  require => Exec['directories'],
-}
-
-exec {'ubuntu':
-  command  => 'sudo chown -R ubuntu:ubuntu /data/',
-  provider => shell,
-  require  => Exec['directories'],
-}
-
-exec {'sudo apt-get update':
-  provider => shell,
-  notify   => Package['nginx'],
 }
 
 package {'nginx':
   ensure   => installed,
   provider => apt,
-  require  => Exec['sudo apt-get update'],
+  require  => Exec['apt_update'],
 }
 
-file_line {'hbnb_static':
+exec {'data':
+  command  => 'sudo mkdir -p /data/web_static/releases/test/; mkdir -p /data/web_static/shared/',
+  provider => shell,
+}
+
+file {'index html':
+  ensure  => file,
+  path    => '/data/web_static/releases/test/index.html',
+  require => Exec['data'],
+  content => "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n",
+}
+
+file {'symbol link':
+  ensure  => link,
+  path    => '/data/web_static/current',
+  target  => '/data/web_static/releases/test',
+  require => File['index html'],
+}
+
+exec {'mode':
+  command  => 'sudo chown -R ubuntu:ubuntu /data/',
+  provider => shell,
+  require  => File['symbol link'],
+}
+
+file_line {'default':
   ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
   line    => "\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}",
   after   => '^\troot /var/www/html;',
-  path    => '/etc/nginx/sites-available/default',
   require => Package['nginx'],
   notify  => Service['nginx'],
 }
 
 service {'nginx':
-  ensure => running,
+  ensure => running
 }

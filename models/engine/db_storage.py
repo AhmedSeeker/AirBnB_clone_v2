01615488d1eee09d838ea73cb8f3e065dbce3ae6
Ahmed <rifkiahmed.91@gmail.com>
2023-10-19 20:19:51 +0000

    Add close() public method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -63,3 +63,7 @@ class DBStorage:
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(Session)
         self.__session = Session()
+
+    def close(self):
+        """recall remove() method on the private session attribute"""
+        self.__session.close()

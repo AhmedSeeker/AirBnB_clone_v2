8bfd39aa1723d2f3e47b761c79a95192209afd55
Ahmed <rifkiahmed.91@gmail.com>
2023-10-20 12:41:25 +0000

    Remove HBNB_TYPE_STORAGE ENV variable

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,7 +4,6 @@ from models import storage
 from models.state import State
 from flask import Flask
 from flask import render_template
-from os import getenv
 
 app = Flask(__name__)
 
@@ -13,10 +12,7 @@ app = Flask(__name__)
 def states():
     """Return list of states"""
     states = storage.all(State).values()
-    return render_template(
-            "8-cities_by_states.html",
-            states=states,
-            storage_type=getenv("HBNB_TYPE_STORAGE"))
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
